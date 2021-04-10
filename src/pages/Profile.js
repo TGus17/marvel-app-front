d4b23fe87f38889a40392853503fa9bae3671fe1
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-04-09 21:07:16 -0300

    :construction: validating token

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { verifyUser } from '../util/verifications';
+// import { requestProcess } from '../service';
 
 function Profile() {
+  const [user, setUser] = useState({});
+  const history = useHistory();
+  
+  const isUserLogged = async () => {
+    const token = JSON.parse(localStorage.getItem('token'));
+    if (!token) return history.push('/');
+    verifyUser(token, history, setUser);
+  }
+
+  useEffect(() => {
+    isUserLogged();
+  }, []);
+
   return (
     <div>
       profile user
